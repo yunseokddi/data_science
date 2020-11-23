6f541e7f6bc7ade6062685e3f9aca35a8111d865
oys5339 <oys5339@naver.com>
2020-11-23 17:50:41 +0900

    padas remind start

---
+++ Python_data_analysis_basic/tutorial_3/data/main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+pd.set_option('precision', 4)
+np.set_printoptions(precision=3)
+
+import warnings
+warnings.filterwarnings('ignore')
+plt.style.use('seaborn-paper')
